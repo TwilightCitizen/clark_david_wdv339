589ed1d78bce579dd46e5736aa95908e8b29cf0b
David Clark <somewhat.esoteric@gmail.com>
2023-02-01 01:49:04 -0500

    Reduced App component to bare minimum.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
 
-function App() {
+// Component
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>Hi! *waves*</p>
   );
 }
+
+// Exports
 
 export default App;
