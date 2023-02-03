0b9b85c4f3f88bedcfdf9bd9dbf27d7b6cd00be6
David Clark <somewhat.esoteric@gmail.com>
2023-02-03 01:46:15 -0500

    Modified Spotify controller to store a generated test token to the Spotify Token Repository on login, with the token middleware pulling it from the repository and adding it to the request.  The status middleware uses the Spotify Token Schema's built in validity check.

--- express-api/controllers/spotify.js
+++ express-api/controllers/spotify.js
@@ -12,6 +12,10 @@ Project Portfolio III
 const path = require('path');
 const dotenv = require('dotenv');
 
+// Application Imports
+
+const spotifyTokenRepository = require('../models/spotifyToken');
+
 // Environment Configuration
 
 dotenv.config({ path: path.join(__dirname, '../.env.local')});
@@ -24,22 +28,45 @@ const {
 
 // Definitions
 
-const token = (req, res, next) => {
-  req.token = { token: 'token' };
+let spotifyTokenID;
+
+const token = async (req, res, next) => {
+  spotifyTokenRepository
+    .then(repository => repository.fetch(spotifyTokenID))
+    
+    .then(spotifyToken => {
+      req.token = spotifyToken
   
-  next();
+      next();
+    });
 };
 
 const login = (req, res, next) => {
-  res.status(200).json({ message: 'login' });
+  spotifyTokenRepository
+    .then(repository => {
+      const spotifyToken = repository.createEntity({
+        access_token: 'Access Token',
+        token_type: 'Token Type',
+        expires_in: new Date().getTime() + 10000, // 10 seconds later
+        refresh_token: 'Refresh Token',
+      });
+    
+      spotifyTokenID = spotifyToken.entityId;
+      
+      return repository.save(spotifyToken);
+    })
+  
+    .then(spotifyToken => {
+      res.status(200).json({ message: 'login' });
+    });
 };
 
 const auth = (req, res, next) => {
-  res.status(200).json({ message: 'auth', ...req.token });
+  res.status(200).json({ message: 'auth', token: req.token });
 };
 
 const status = (req, res, next) => {
-  res.status(200).json({ message: 'status', ...req.token });
+  res.status(200).json({ valid: req.token.isValid });
 };
 
 const search = (req, res, next) => {
