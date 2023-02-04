32b1ae0cbc92268c022c08681f32922777baba77
David Clark <somewhat.esoteric@gmail.com>
2023-02-04 11:43:26 -0500

    Imported Fetch and Query String.  Added API environment variables to configuration.  Defined authorization headers, fetch options, urls, and skeleton fetch functions.  Modified login middleware to redirect to Spotify for authorization.

--- express-api/controllers/spotify.js
+++ express-api/controllers/spotify.js
@@ -11,6 +11,8 @@ Project Portfolio III
 
 import path from 'path';
 import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import queryString from 'query-string';
 
 // Application Imports
 
@@ -23,6 +25,10 @@ dotenv.config();
 const local = dotenv.config({ path: path.resolve(process.cwd(), './.env.local') });
 
 const {
+  API_SCHEME,
+  API_HOST,
+  API_PORT,
+  API_BASE_URL,
   SPOTIFY_CLIENT_ID,
   SPOTIFY_CLIENT_SECRET,
 } = {
@@ -33,6 +39,31 @@ const {
 // Definitions
 
 let spotifyTokenID;
+
+const authPayload = new Buffer
+  .from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`)
+  .toString('base64');
+
+const redirectUrl = `${API_SCHEME}://${API_HOST}:${API_PORT}${API_BASE_URL}/auth`;
+const authUrl = 'https://accounts.spotify.com/authorize';
+const tokenUrl = 'https://accounts.spotify.com/api/token';
+
+const fetchOptions = {
+  method: 'post',
+  
+  headers: {
+    'Authorization': `Basic ${authPayload}`,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+};
+
+const fetchAuthorize = () => {
+
+};
+
+const fetchToken = () => {
+
+};
 
 const token = async (req, res, next) => {
   spotifyTokenRepository
@@ -46,21 +77,30 @@ const token = async (req, res, next) => {
 };
 
 const login = (req, res, next) => {
-  const spotifyToken = spotifyTokenRepository.createEntity({
-    access_token: 'Access Token',
-    token_type: 'Token Type',
-    expires_in: new Date().getTime() + 10000, // 10 seconds later
-    refresh_token: 'Refresh Token',
-  })
+  // const spotifyToken = spotifyTokenRepository.createEntity({
+  //   access_token: 'Access Token',
+  //   token_type: 'Token Type',
+  //   expires_in: new Date().getTime() + 10000, // 10 seconds later
+  //   refresh_token: 'Refresh Token',
+  // })
+  //
+  // spotifyTokenRepository
+  //   .save(spotifyToken)
+  //
+  //   .then(id => {
+  //     spotifyTokenID = id;
+  //
+  //     res.status(200).json({ message: 'login' });
+  //   });
   
-  spotifyTokenRepository
-    .save(spotifyToken)
-    
-    .then(id => {
-      spotifyTokenID = id;
-      
-      res.status(200).json({ message: 'login' });
-    });
+  const query = queryString.stringify({
+    client_id: SPOTIFY_CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: redirectUrl,
+    state: 'unimplemented'
+  });
+  
+  res.redirect(`${authUrl}?${query}`);
 };
 
 const auth = (req, res, next) => {
