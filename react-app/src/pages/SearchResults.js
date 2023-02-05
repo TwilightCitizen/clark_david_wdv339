3f45e61da96cf11915e917d024a9756cb82b6083
David Clark <somewhat.esoteric@gmail.com>
2023-02-05 13:12:38 -0500

    Defined skeleton Search Results page that will display Spotify search results, if any, to the user.

---
+++ react-app/src/pages/SearchResults.js
@@ -0,0 +1,28 @@
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
+
+// Library Imports
+
+
+
+// Application Imports
+
+
+
+// Definition
+
+const SearchResults = () => {
+  return (
+    <p>SearchResults Page</p>
+  );
+};
+
+// Exports
+
+export default SearchResults;
