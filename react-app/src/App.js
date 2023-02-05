a2bbcd5ae54618a15b47d535ea2500cf223389fe
David Clark <somewhat.esoteric@gmail.com>
2023-02-05 14:20:22 -0500

    Refactored App component, providing and exporting contexts for logged in and search results states, and routing with the Search Results page as a protected route.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,16 +7,53 @@ Section	01
 Project Portfolio III
 */
 
-// Component
+// Library Imports
+
+import { createContext, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Application Imports
+
+import ProtectedRoute from './components/ProtectedRoute';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import SearchResults from './pages/SearchResults';
+
+// Definitions
+
+const LoggedInContext = createContext(null);
+const SearchResultsContext = createContext(null);
+
+const ProtectedSearchResults = () =>
+  <ProtectedRoute>
+    <SearchResults />
+  </ProtectedRoute>
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [searchResults, setSearchResults] = useState({});
+  
   return (
-    <div className="flex flex-wrap items-center justify-center vh-100">
-      <p>Hi! *waves*</p>
-    </div>
+    <LoggedInContext.Provider value={[loggedIn, setLoggedIn]}>
+      <SearchResultsContext.Provider value={[searchResults, setSearchResults]}>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/search-results" element={<ProtectedSearchResults />} />
+            <Route path="/" exact element={<ProtectedSearchResults />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
+      </SearchResultsContext.Provider>
+    </LoggedInContext.Provider>
   );
 }
 
 // Exports
 
 export default App;
+
+export {
+  LoggedInContext,
+  SearchResultsContext
+}
