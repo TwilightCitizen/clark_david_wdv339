de674fef1a831498870068c905bc0c1793633aea
David Clark <somewhat.esoteric@gmail.com>
2023-02-06 18:47:43 -0500

    Modified Not Found page template to render the green variant of the Spotify logo, an exclamation, and a navigation link back to a functioning page.

--- react-app/src/pages/NotFound.js
+++ react-app/src/pages/NotFound.js
@@ -9,17 +9,37 @@ Project Portfolio III
 
 // Library Imports
 
-
+import { Link } from 'react-router-dom';
 
 // Application Imports
 
-
+import Logo from '../components/Logo';
 
 // Definition
 
 const NotFound = () => {
   return (
-    <p>NotFound Page</p>
+    <div className="flex flex-column flex-grow-1 justify-center items-center">
+      <Logo variant="green" />
+      <h1 className="f3 f2-m f1-l lh-copy">Error 404 - Not Found! &#128559;</h1>
+      
+      <p className="f5 f3-m f2-l lh-copy tc w-80 w-50-l">
+        OMG...  What did you do?  You broke it!
+      </p>
+  
+      <p className="f5 f3-m f2-l lh-copy tc w-80 w-50-l">
+        Just kidding!  &#128514;  Tap the button below to get somewhere normal.
+      </p>
+      
+      <Link
+        to="/"
+        className="no-underline"
+      >
+        <p className="white bg-dark-green pa2 br4 no-underline hover-bg-light-green hover-black-80">
+          The Button Below
+        </p>
+      </Link>
+    </div>
   );
 };
 
