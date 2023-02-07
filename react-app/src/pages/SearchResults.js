87e023d1d79a8b1431cb5557de950ad5c7a4901c
David Clark <somewhat.esoteric@gmail.com>
2023-02-06 22:20:13 -0500

    Modified Search Results page to conditionally display the green Spotify logo and various messages, dependent upon the search results state.

--- react-app/src/pages/SearchResults.js
+++ react-app/src/pages/SearchResults.js
@@ -15,23 +15,25 @@ import { useContext } from 'react';
 
 import WithLayout from './WithLayout';
 import { SearchResultsContext } from '../App';
-
+import Logo from '../components/Logo';
 
 // Definition
 
 const SearchResults = () => {
-  const [searchResults] = useContext(SearchResultsContext)
+  const [searchResults] = useContext(SearchResultsContext);
   
   return (
-    <>
+    <div className="flex flex-column flex-grow-1 justify-center items-center">
+      {searchResults.data ? null : <Logo variant="green" />}
+      
       {searchResults.data
-        ? <p>{JSON.stringify(searchResults)}</p>
+        ? <p className="tc w-80 w-50-l overflow-hidden">{JSON.stringify(searchResults.data)}</p>
         : searchResults.error
-        ? <p>A horrible error corrupted your search!</p>
+        ? <p className="tc w-80 w-50-l">A horrible error corrupted your search!  It's probably your fault.  The guy that made this app is like really REALLY good at coding...</p>
         : searchResults.pending
-        ? <p>Hang on while we search for that stuff...</p>
-        : <p>Why not try searching for something already?</p>}
-    </>
+        ? <p className="tc w-80 w-50-l">Hang on while we search for that stuff!  Hopefully you didn't search for something hard to find...</p>
+        : <p className="tc w-80 w-50-l">Why not try searching for something already? Artists, albums, and tracks aren't going to just search for themselves after all!</p>}
+    </div>
   );
 };
 
