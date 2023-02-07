3edfd7a36151458933019281859e47afaa6c2bc2
David Clark <somewhat.esoteric@gmail.com>
2023-02-06 21:22:39 -0500

    Refactored Search Bar component with an indeterminate search progress that overlays a search term input and search button.  Search terms are maintained in state, and the search button now searches with those terms instead of hard-coded values.

--- react-app/src/components/SearchBar.js
+++ react-app/src/components/SearchBar.js
@@ -9,7 +9,7 @@ Project Portfolio III
 
 // Library Imports
 
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 // Application Imports
 
@@ -20,11 +20,13 @@ import useSpotifyApi from '../hooks/useSpotifyApi';
 
 const SearchBar = () => {
   const [, setSearchResults] = useContext(SearchResultsContext);
+  const [searchTerms, setSearchTerms] = useState('');
   
   const {
     getSearch: {
       getSearchData,
       getSearchError,
+      getSearchIsPending,
       getSearchRun
     }
   } = useSpotifyApi();
@@ -34,23 +36,46 @@ const SearchBar = () => {
     if (getSearchError) setSearchResults(null);
   }, [getSearchData, getSearchError]);
   
+  const searchDisabled = getSearchIsPending || searchTerms === '';
+  
   return (
-    <form className="flex flex-row flex-grow-1 justify-center ml1 ml2-m ml3-l">
+    <form className="flex flex-row flex-grow-1 h2 justify-center ml1 ml2-m ml3-l relative">
+      <div
+        className="h2 bg-green br4 shadow-1 w-100 progress-bar absolute o-50"
+        
+        style={{
+          zIndex: +2,
+          display: `${getSearchIsPending ? "block" : "none"}`
+        }}
+      >
+      </div>
+      
       <input
         type="text"
         placeholder="Enter an artist, album, or track name, and tap search. &rarr;"
-        className="flex-grow-1 pa1 br4 bw0 tc"
+        className="flex-grow-1 pa1 br4 bw0 tc outline-0"
+        value={searchTerms}
+        onChange={event => setSearchTerms(event.target.value)}
         style={{ zIndex: +1 }}
+        disabled={getSearchIsPending}
       />
       
       <button
         type="button"
-        className="flex-grow-0 br4 br--right bw0 bg-light-green black-80 pl3 hover-bg-black-80 hover-white"
         style={{ marginLeft: "-0.75em" }}
+        disabled={searchDisabled}
+        
+        className={
+          `flex-grow-0 br4 br--right bw0 pl3 outline-0
+          
+          ${searchDisabled ? '' :
+            'bg-light-green black-80 hover-bg-black-80 hover-white'
+          }`
+        }
         
         onClick={event => {
           event.preventDefault();
-          getSearchRun('Sidewalks and Skeletons');
+          getSearchRun(searchTerms);
         }}
       >
         Search
