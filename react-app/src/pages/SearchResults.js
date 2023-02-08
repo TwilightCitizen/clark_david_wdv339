b4e41bb280b8ea6c64cc2b7b180b118434a55541
David Clark <somewhat.esoteric@gmail.com>
2023-02-07 20:12:03 -0500

    Refactored Search Results page to display Spotify Cards components.

--- react-app/src/pages/SearchResults.js
+++ react-app/src/pages/SearchResults.js
@@ -16,6 +16,7 @@ import { useContext } from 'react';
 import WithLayout from './WithLayout';
 import { SearchResultsContext } from '../App';
 import Logo from '../components/Logo';
+import SpotifyCards from '../components/SpotifyCards';
 
 // Definition
 
@@ -23,16 +24,42 @@ const SearchResults = () => {
   const [searchResults] = useContext(SearchResultsContext);
   
   return (
-    <div className="flex flex-column flex-grow-1 justify-center items-center">
-      {searchResults.data ? null : <Logo variant="green" />}
+    <div className='flex flex-column flex-grow-1 justify-center items-center'>
+      {searchResults.data ? null : <Logo variant='green' />}
       
       {searchResults.data
-        ? <p className="tc w-80 w-50-l overflow-hidden">{JSON.stringify(searchResults.data)}</p>
+        ? <>
+          <SpotifyCards
+            title="Artists"
+            fromSpotifyJsonData={searchResults.data.artists}
+          />
+    
+          <SpotifyCards
+            title="Albums"
+            fromSpotifyJsonData={searchResults.data.albums}
+          />
+    
+          <SpotifyCards
+            title="Tracks"
+            fromSpotifyJsonData={searchResults.data.tracks}
+          />
+        </>
+        
         : searchResults.error
-        ? <p className="tc w-80 w-50-l">A horrible error corrupted your search!  It's probably your fault.  The guy that made this app is like really REALLY good at coding...</p>
+        
+        ? <p className='tc w-80 w-50-l'>
+            A horrible error corrupted your search!  It's probably your fault.  The guy that made this app is like really, REALLY good at coding, so...
+        </p>
+        
         : searchResults.pending
-        ? <p className="tc w-80 w-50-l">Hang on while we search for that stuff!  Hopefully you didn't search for something hard to find...</p>
-        : <p className="tc w-80 w-50-l">Why not try searching for something already? Artists, albums, and tracks aren't going to just search for themselves after all!</p>}
+        
+        ? <p className='tc w-80 w-50-l'>
+            Hang on while we search for that stuff!  Hopefully you didn't search for something hard to find...
+        </p>
+        
+        : <p className='tc w-80 w-50-l'>
+            Why not try searching for something already? Artists, albums, and tracks aren't going to just search for themselves after all!
+        </p>}
     </div>
   );
 };
