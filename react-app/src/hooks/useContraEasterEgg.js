ed3f342724bbba29bd1c67b2951a08f7e3d1c6a3
David Clark <somewhat.esoteric@gmail.com>
2023-02-09 23:24:09 -0500

    Implemented Contra Easter Egg custom hook that uses useReducer to check the state of search terms against Up, Up, Down, Down, Left, Right, Left, Right, B, A--case insensitive, to fire onFulfilled--if it exists--in pages or components that use it.

---
+++ react-app/src/hooks/useContraEasterEgg.js
@@ -0,0 +1,91 @@
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
+
+// Library Imports
+
+import { useEffect, useReducer } from 'react';
+
+// Definitions
+
+const fiftyLivesCodes = [
+  'up', 'up',
+  'down', 'down',
+  'left', 'right',
+  'left', 'right',
+  'b', 'a'
+];
+
+const actions = {
+  searched_for: 'searched_for',
+  reset: 'reset'
+};
+
+const initialState = {
+  codesRemaining: fiftyLivesCodes,
+  searched: []
+};
+
+const contraReducer = (state, action) => {
+  if (action.type === actions.searched_for) {
+    const entry = state.codesRemaining[0];
+    
+    if (action
+      .terms
+      .toLowerCase()
+      .includes(entry)
+    ) {
+      console.log(
+        action
+        .terms
+        .toLowerCase()
+        .includes(entry)
+      )
+      
+      console.log({
+        codesRemaining: state.codesRemaining.slice(1),
+        searched: [...state.searched, action.terms]
+      })
+      
+      return ({
+        codesRemaining: state.codesRemaining.slice(1),
+        searched: [...state.searched, action.terms]
+      });
+    }
+    
+    return initialState;
+  }
+  
+  return initialState;
+};
+
+const useContraEasterEgg = onFulfilled => {
+  const [state, dispatch] = useReducer(
+    contraReducer, initialState
+  );
+  
+  useEffect(() => {
+    if (!state.codesRemaining.length && onFulfilled) {
+      onFulfilled();
+      dispatch({ type: actions.reset });
+    }
+  }, [state]);
+  
+  const searchedFor = terms => {
+    dispatch({
+      type: actions.searched_for,
+      terms: terms
+    });
+  };
+  
+  return [searchedFor];
+};
+
+// Exports
+
+export default useContraEasterEgg;
