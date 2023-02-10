6efbf99f43e600766068c7d15730c92bf5e06b50
David Clark <somewhat.esoteric@gmail.com>
2023-02-09 23:26:56 -0500

    Modifed SearchBar to use the Contra Easter Egg custom hook, alerting of 50 lives when fulfilled and setting searchedFor on search.

--- react-app/src/components/SpotifyCard.js
+++ react-app/src/components/SpotifyCard.js
@@ -18,6 +18,8 @@ Project Portfolio III
 // Definition
 
 const SpotifyCard = ({ fromSpotifyJsonData: jsonData }) => {
+  console.log(jsonData);
+  
   return (
     <a
       className="flex flex-column w-100 w-40-m w-30-l bg-dark-green pa1 pa2-m pa3-l br4 ma1 ma2-m ma3-l no-underline white hover-bg-light-green hover-black-80"
@@ -27,7 +29,7 @@ const SpotifyCard = ({ fromSpotifyJsonData: jsonData }) => {
     >
       <img
         className="w-100 flex-grow-1 br4"
-        src={jsonData.album?.images[0].url ?? jsonData.images[0].url}
+        src={jsonData.album?.images[0].url ?? jsonData.images[0]?.url ?? 'spotify-logo-green.png'}
         alt="Artist or Album Artwork"
       />
       
