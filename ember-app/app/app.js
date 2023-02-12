392c4bc73bd362bbc070a9cc36ce6f7b4a1f3adf
David Clark <somewhat.esoteric@gmail.com>
2023-02-11 23:19:45 -0500

    Added header block comment and section comment headings, reorganizing the module with separate definition and exports.

--- ember-app/app/app.js
+++ ember-app/app/app.js
@@ -1,12 +1,32 @@
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
+
+// Library Imports
+
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
-import config from 'clark-david-wdv339-ember-app/config/environment';
 
-export default class App extends Application {
+// Application Imports
+
+import config from 'ember-app/config/environment';
+
+// Definition
+
+class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
 }
 
 loadInitializers(App, config.modulePrefix);
+
+// Exports
+
+export default App;
