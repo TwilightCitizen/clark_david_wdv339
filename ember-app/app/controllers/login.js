16a1d4802421983fe124483edab71af3b3e1e25e
David Clark <somewhat.esoteric@gmail.com>
2023-02-12 16:42:27 -0500

    Added Login Controller again.  Documentation is very confusing.

---
+++ ember-app/app/controllers/login.js
@@ -0,0 +1,21 @@
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
+
+// Library Imports
+
+import Controller from '@ember/controller';
+
+// Definition
+
+class LoginController extends Controller {}
+
+
+// Exports
+
+export default LoginController;
