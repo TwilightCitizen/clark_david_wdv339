d4bfca6f4b6beb3169ddc58927baaf0c16215b07
David Clark <somewhat.esoteric@gmail.com>
2023-02-12 16:52:24 -0500

    Implemented login logic in Spotify API Service with Ember's polyfill for fetch since it just dies with Node Fetch for some reason.

--- ember-app/app/services/spotify-api.js
+++ ember-app/app/services/spotify-api.js
@@ -10,11 +10,42 @@ Project Portfolio III
 // Library Imports
 
 import Service from '@ember/service';
+import fetch from 'fetch';
 
-// Definition
+// Application Imports
 
-class SpotifyApiService extends Service {}
+import config from 'ember-app/config/environment';
+
+// Definitions
+
+const { spotifyUrl, scheme, host, port, baseUrl } = config.API;
+
+const API_URL = [scheme, '://', host, ':', port, baseUrl, spotifyUrl].join('');
+
+const loginUrl = `${API_URL}/login`;
+const statusUrl = `${API_URL}/status`;
+const getHeaders = { Accept: 'application/json' };
+
+class SpotifyApiService extends Service {
+  get loggedIn() {
+    return (async () => {
+      try {
+        const response = await fetch(statusUrl, {
+          headers: getHeaders,
+        });
+
+        const data = await response.json();
+
+        return data.valid;
+      } catch (_error) {
+        return false;
+      }
+    })();
+  }
+}
 
 // Exports
 
 export default SpotifyApiService;
+
+export { loginUrl };
