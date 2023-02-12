501a5af622582cf7391cf48875a4243152e78316
David Clark <somewhat.esoteric@gmail.com>
2023-02-11 22:23:59 -0500

    Added header block comment and section comment headings, reorganizing the module with separate definition and exports.

--- ember-app/app/router.js
+++ ember-app/app/router.js
@@ -1,9 +1,29 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'clark-david-wdv339-ember-app/config/environment';
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
 
-export default class Router extends EmberRouter {
+// Library Imports
+
+import EmberRouter from '@ember/routing/router';
+
+// Application Imports
+
+import config from 'ember-app/config/environment';
+
+// Definition
+
+class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
 Router.map(function () {});
+
+// Exports
+
+export default Router;
