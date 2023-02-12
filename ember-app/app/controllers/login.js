0991f1e3083a7fe91b5cb6ded23d87a572a70199
David Clark <somewhat.esoteric@gmail.com>
2023-02-12 14:45:02 -0500

    Added skeleton controller for the Login route.

---
+++ ember-app/app/controllers/login.js
@@ -0,0 +1,20 @@
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
+
+// Library Imports
+
+import Controller from '@ember/controller';
+
+// Definition
+
+class LoginController extends Controller {}
+
+// Exports
+
+export default LoginController;
