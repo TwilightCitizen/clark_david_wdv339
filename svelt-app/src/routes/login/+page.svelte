ec5936db807bc8ec88bad98c39534571369d9979
David Clark <somewhat.esoteric@gmail.com>
2023-02-14 21:40:31 -0500

    Added content and Tachyons styling to Login route.

--- svelt-app/src/routes/login/+page.svelte
+++ svelt-app/src/routes/login/+page.svelte
@@ -11,11 +11,19 @@ Project Portfolio III
   // Application Imports
 
   import { loginUrl } from '$lib/spotify-api.js';
-  
-  // Exports
-  
-  export { loginUrl };
 </script>
 
-<h1>Login</h1>
-<a href="{loginUrl}">Login</a>
+<div class="flex flex-column flex-grow-1 justify-center items-center">
+  <h1 class="f3 lh-copy">Please Login</h1>
+  
+  <p class="tc w-80 w-50-l">
+    If you don't log in, nothing else will happen, and that wouldn't be that much fun now, would it?  Just tap the button below to make fun stuff happen.  Go ahead.  You know you want to...
+  </p>
+  
+  <a
+    href={loginUrl}
+    class="white bg-dark-green pa2 br4 no-underline hover-bg-light-green hover-black-80"
+  >
+    Login
+  </a>
+</div>
