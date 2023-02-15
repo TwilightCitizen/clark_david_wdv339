6bd9f7a9f66e14ff9a162029f967dda9076a4719
David Clark <somewhat.esoteric@gmail.com>
2023-02-14 19:27:22 -0500

    Modified the route Layout, importing and using the Spotify Logo component in the header and applying Tachyons styling to the header.

--- svelt-app/src/routes/+layout.svelte
+++ svelt-app/src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
+  // Library Imports
+  
   import { page } from '$app/stores';
+  
+  // Application Imports
+  
+  import SpotifyLogo from '$lib/SpotifyLogo.svelte';
 </script>
 
 {#if !$page.error}
-  <header>
-    <p>Header</p>
+  <header class="bg-dark-green flex flex-row flex-grow-0 items-center pa1 pa2-m pa3-l">
+    <SpotifyLogo variant="white" />
   </header>
 {/if}
 
