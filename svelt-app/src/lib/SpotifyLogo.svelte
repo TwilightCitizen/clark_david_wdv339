30796d662201a1b899e93c580b3358660eea013f
David Clark <somewhat.esoteric@gmail.com>
2023-02-14 19:26:10 -0500

    Implemented Spotify Logo component that encapsulates variants of the Spotify Logo: green and white.

---
+++ svelt-app/src/lib/SpotifyLogo.svelte
@@ -0,0 +1,24 @@
+<!--
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+-->
+
+<script>
+  // Definitions
+  
+  let variant;
+  
+  // Exports
+  
+  export { variant };
+</script>
+
+<img
+  src={`spotify-logo-${variant}.png`}
+  alt={`Spotify Logo ${variant}`}
+  class="h2 h3-l flex-grow-0"
+/>
