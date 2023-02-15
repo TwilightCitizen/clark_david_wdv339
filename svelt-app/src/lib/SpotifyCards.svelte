ee56a56259c861208206a410d72368d254066b58
David Clark <somewhat.esoteric@gmail.com>
2023-02-14 20:59:10 -0500

    Implemented Spotify Cards component.

---
+++ svelt-app/src/lib/SpotifyCards.svelte
@@ -0,0 +1,40 @@
+<!--
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+-->
+
+<script>
+  // Imports
+  
+  import SpotifyCard from "$lib/SpotifyCard.svelte";
+  
+  // Definitions
+  
+  let title
+  let jsonData;
+  
+  // Exports
+  
+  export { jsonData, SpotifyCard };
+</script>
+
+<div class="flex flex-column w-100">
+  <h1 class="tc f3 lh-copy">{title}</h1>
+  
+  <div class="flex flex-row flex-wrap w-100 justify-center">
+    {#each jsonData.items as item, index}
+      <SpotifyCard
+        key={index}
+        @jsonData={item}
+      />
+    {:else}
+      <p class="tc w-80 w-50-l">
+        Spotify returned exactly zero, nada, zilch {title.toLowerCase()}}!  Maybe you searched for something stupid?  &#128556;
+      </p>
+    {/each}
+  </div>
+</div>
