e04bf084e971ea4a4594a073bde45cce012053c5
David Clark <somewhat.esoteric@gmail.com>
2023-02-18 15:16:29 -0500

    Created skeleton Spotify Card and Spotify Cards components.

---
+++ vue-app/src/components/SpotifyCards.vue
@@ -0,0 +1,18 @@
+<!--
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+-->
+
+<script>
+  export default {
+    name: 'SpotifyCards',
+  }
+</script>
+
+<template>
+
+</template>
