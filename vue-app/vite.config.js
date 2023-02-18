43486f5f9debee755e29b3e4600f0f4355aa4585
David Clark <somewhat.esoteric@gmail.com>
2023-02-18 14:59:49 -0500

    Changed default port to 3000.

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -1,14 +1,40 @@
-import { fileURLToPath, URL } from 'node:url'
+/*
+David A. Clark, Jr.
+Student #0004796375
+Class WDV339-O
+Term C202302
+Section	01
+Project Portfolio III
+*/
 
+// Library Imports
+
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// Definition
 // https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [vue()],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  
+  server: {
+    port: 3000,
+    strictPort: false,
+  },
+  
+  preview: {
+    port: 3000,
+    strictPort: false,
   }
-})
+});
+
+// Exports
+
+export default config;
