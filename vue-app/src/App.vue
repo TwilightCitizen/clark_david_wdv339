aa044ddbdc222d7ead2c23a9e5aaeaeb67df7ea6
David Clark <somewhat.esoteric@gmail.com>
2023-02-18 16:44:25 -0500

    Added Search Bar component to header.

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -15,11 +15,13 @@ Project Portfolio III
   // Application Imports
 
   import SpotifyLogo from '@/components/SpotifyLogo.vue';
+  import SearchBar from '@/components/SearchBar.vue';
 </script>
 
 <template>
-  <header class='bg-dark-green flex flex-row flex-grow-0 items-center pa1 pa2-m pa3-l'>
+  <header class="bg-dark-green flex flex-row flex-grow-0 items-center pa1 pa2-m pa3-l">
     <SpotifyLogo variant="white" />
+    <SearchBar />
   </header>
 
   <RouterView />
