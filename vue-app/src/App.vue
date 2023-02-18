785c3921a680bfa5a3cc8251b32f4ca1896defa3
David Clark <somewhat.esoteric@gmail.com>
2023-02-18 14:53:43 -0500

    Applied Tachyons styling to header.

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -18,7 +18,7 @@ Project Portfolio III
 </script>
 
 <template>
-  <header>
+  <header class="bg-dark-green flex flex-row flex-grow-0 items-center pa1 pa2-m pa3-l">
     <p>Header</p>
   </header>
 
